94b5a155d777dd642b72da2bac7ab4854653fa2d
yewhen22888 <yewhen22888@gmail.com>
2022-11-06 11:07:26 +0200

    notcolor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,11 @@ import s from './Header.module.css'
 
 const Header = () => {
     return (
-        <div>
-            <header className={s.header}>
-                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/BMW.svg/2048px-BMW.svg.png' />
-            </header>
-            <div>
-                <img src='https://www.bmw.ru/content/dam/bmw/marketRU/bmw_ru/topics/fascination-bmw/bmw-m-logo-and-colors/bmw-m-logo-and-colors.jpg' /
-    </div>
-        </div>
+
+        <header className={s.logo}>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/BMW.svg/2048px-BMW.svg.png' />
+        </header>
+
     )
 }
 
