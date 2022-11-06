c449e7685f0dfd5e5e4a8425821710f3403e81d4
pendlyshakilly <pendlyshakilly@gmail.com>
2022-11-06 16:14:58 +0200

    debug_error

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,18 +1,18 @@
 import Posts from './MyPosts/Posts';
-import s from './Profile.module.css'
+import s from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className={s.content}>
-      <img src='https://static.autox.com/uploads/2021/01/BMW-3-Series-Gran-Limousine-launched-in-India-500x261.jpg  ' />
-      <div>
-        ava + diskription
+    <div className={s.background}>
+      <div className={s.content}>
+        <img src='https://static.autox.com/uploads/2021/01/BMW-3-Series-Gran-Limousine-launched-in-India-500x261.jpg  ' />
+        <div>ava + diskription</div>
       </div>
       <div>
         <Posts />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Profile;
