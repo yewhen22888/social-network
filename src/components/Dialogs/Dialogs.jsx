93c6edcda7479692c8e5e67105c72cd31219598c
yewhen22888 <yewhen22888@gmail.com>
2022-11-12 20:00:52 +0200

    lesson 21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,32 @@
-import s from'./Dialogs.module.css'
+import s from './Dialogs.module.css'
 
 const Dialogs = () => {
-    return(
-        <div>
-            Dialogs
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Illy
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Viktor
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimon
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Sveta
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    hi how are you?
+                </div>
+                <div className={s.messages}>
+                    go to football today
+                </div>
+
+            </div>
         </div>
     )
 }
- export default Dialogs
+export default Dialogs
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,31 @@
-import s from'./Navbar.module.css'
+import { NavLink } from 'react-router-dom';
+import s from './Navbar.module.css'
 
 const NavBar = () => {
-    return(
-        <nav className={s.Nav}>
-        <div className={s.item} >
-          <a href='/Profile'>Profile</a>
-        </div>
-        <div className={s.item}>
-          <a href='/Dialogs'>Message</a>
-        </div>
-        <div className={s.item}>
-          <a href='Video'>Video</a>
-        </div>
-        <div className={s.item}>
-          <a href='/Music'>Music</a>
-        </div>
-        <div className={s.item} >
-          <a href='/Option'>Option</a>
-        </div>
-      </nav>
-    )
+  return (
+    <nav className={s.Nav}>
+      <div className={s.item} >
+        <NavLink to='/Profile' className={navData => navData.isActive ? s.active : s.item}
+        >Profile</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to='/Dialogs' className={navData => navData.isActive ? s.active : s.item}
+        >Message</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to='Video' className={navData => navData.isActive ? s.active : s.item}
+        >Video</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to='/Music' className={navData => navData.isActive ? s.active : s.item}
+        >Music</NavLink>
+      </div>
+      <div className={s.item} >
+        <NavLink to='/Option' className={navData => navData.isActive ? s.active : s.item}
+         >Option</NavLink>
+      </div>
+    </nav>
+  )
 }
 
 export default NavBar;
