bb42dabe92c1ce84d9759d3ba990490bbdfb0c18
yewhen22888 <yewhen22888@gmail.com>
2023-03-21 22:08:18 +0200

    lk

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,23 +3,23 @@ import s from './MyPosts.module.css'
 import React from 'react';
 
 
-
 const Posts = (props) => {
 
-  let postelement = props.posts.map(el => <Post message={el.message} likeCounts={el.likeCounts} />)
+    let postelement = props.posts.map(el => <Post message={el.message} likeCounts={el.likeCounts}/>)
 
-  let RefaddPost = React.createRef();
+    let RefaddPost = React.createRef();
 
-  let onPostChange = () => {
-    let text = RefaddPost.current.value;
-    props.updateNewPostChange(text)
+    let onPostChange = () => {
+        let text = RefaddPost.current.value;
+        props.updateNewPostChange(text)
 
-  };
+    };
 
-  let addPost = () => {
-    props.addPost()
-  };
+    let addPost = () => {
+        props.addPost()
+    };
 
+<<<<<<< HEAD
   return (
     <div className={s.postBar}>
       <div>
@@ -34,6 +34,24 @@ const Posts = (props) => {
       </div>
     </div >
   )
+=======
+
+    return (
+        <div className={s.postBar}>
+            <div>
+                My post
+            </div>
+            <textarea placeholder='YourText' onChange={onPostChange} ref={RefaddPost}
+                      value={props.newPostText}></textarea>
+            <div>
+                <button onClick={addPost}>New Post</button>
+            </div>
+            <div className='posts'>
+                {postelement}
+            </div>
+        </div>
+    )
+>>>>>>> f48d723ba3db62a0143a7084a8acc87adce37249
 }
 
 export default Posts;
