afd43b3f804bb42d8207583498532e0c7f47d94d
pemdlyshakilly <pendlyshakilly@gmail.com>
2023-04-09 11:40:49 +0200

    Error_Fix

--- src/components/Users/UsersPage.jsx
+++ src/components/Users/UsersPage.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import s from './UsersPage.module.css'
 
+
 const UsersPage = (props) => {
-    debugger
+
     if (props.Users.length === 0) {
         props.SetUsers(
             [{ id: 1, img: "https://hips.hearstapps.com/hmg-prod/images/p90445339-lowres-1638019542.jpg", follow: true, FullName: 'Dimon', status: 'I like Js', location: { contry: 'Belarus', city: 'Minsk' } },
@@ -11,26 +12,34 @@ const UsersPage = (props) => {
             { id: 4, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBEhms4bd2CScG9sIYWGavVT5wvoJ37Mv5XA&usqp=CAU", follow: true, FullName: 'Vasja', status: 'Stutututu', location: { contry: 'Japan', city: 'Kawasaki' } }]
         )
     }
-    return <div >
+    return <div>
         {
-            props.Users.map(u => <div key={u.id} className={s.users_wrapper}>
-                <div className={s.ava}>
-                    <img src={u.img} />
-                    <div >
-                        {u.follow ?
-                            <button onClick={() => { props.UnFollow(u.id) }}>UnFollow</button> :
-                            <button onClick={() => { props.Follow(u.id) }} >Follow</button>}
+            props.Users.map(u => {
+                return <div className={s.Users_Main} key={u.id}>
+                    <div className={s.users_wrapper}>
+                        <div className={s.ava}>
+                            <img src={u.img}/>
+                            <div>
+                                {u.follow ?
+                                    <button onClick={() => {
+                                        u.UnFollow(u.id)
+                                    }}>UnFollow</button> :
+                                    <button onClick={() => {
+                                        u.Follow(u.id)
+                                    }}>Follow</button>}
 
+                            </div>
+                        </div>
+                        <div className={s.text}>
+                            <div>{u.FullName}</div>
+                            <div>{u.status}</div>
+                            <div>{u.location.city}</div>
+                            <div>{u.location.contry}</div>
+
+                        </div>
                     </div>
                 </div>
-                <div className={s.text}>
-                    <div>{u.FullName}</div>
-                    <div>{u.status}</div>
-                    <div>{u.location.city}</div>
-                    <div>{u.location.contry}</div>
-
-                </div>
-            </div>)
+            })
         }
     </div>
 }
